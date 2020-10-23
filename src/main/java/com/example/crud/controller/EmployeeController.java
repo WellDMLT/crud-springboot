6f14055a6263f6c523daf914b738df1d0ac327e7
WellingtonApCardoso <wellington.cardoso18@gmail.com>
2020-10-23 16:36:43 -0300

    second commit

--- src/main/java/com/example/crud/controller/EmployeeController.java
+++ src/main/java/com/example/crud/controller/EmployeeController.java
@@ -1,7 +1,23 @@
 package com.example.crud.controller;
 
+import com.example.crud.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("listEmployees", employeeService.getAllEmployees());
+        return "index";
+    }
 }
--- src/main/java/com/example/crud/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/crud/service/EmployeeServiceImpl.java
@@ -4,9 +4,13 @@ package com.example.crud.service;
 import com.example.crud.model.Employee;
 import com.example.crud.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class  EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
